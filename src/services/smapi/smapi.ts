898ffb350217362c1070c84c0c4690e54cab28b1
Aviv Eyal <aviveyal07@gmail.com>
2018-12-10 17:47:47 +0200

    Additional comments and slight refactoring

--- src/services/smapi/smapi.ts
+++ src/services/smapi/smapi.ts
@@ -5,50 +5,56 @@ import {IUtils} from "./utils"
 
 export interface ISpacemeshApi {
 
-    // API version
+    // Returns the API version
     Version(): Promise<string>;
 
-    // API Utils object
+    // Returns the API Utils object
     Utils(): Promise<IUtils>;
 
-    // Set a provider. A provider provides the api
+    // Sets a provider. A provider provides the spacemesh api over a connection
     SetProvider(IProvider): Promise<boolean>;
 
-    // Get the current set provider
-    GetProvider(): Promise<IProvider>;
+    // Get the current provider if one is set, nil otherwise
+    GetProvider(): Promise<IProvider | null>;
 
-    // For debugging - the protocol version implemented by the provider
+    // Returns the provider version
     GetProviderVersion(): Promise<string>;
 
-    // Get the provider given by the contexst
+    // Gets the provider given by the contexst that can be set
     GetGivenProvider(): Promise<IProvider>;
 
-    // Returns true if mesh is not yet ready for queries (syncing)
-    IsSyncing(): Promise<boolean>;
+    // Returns false if mesh is not yet ready for queries e.g it is still syncing
+    IsReady(): Promise<boolean>;
 
-    // Current blockmesh layer info
+    // Returns the current mesh layer info
     GetLatestLayerNumber(): Promise<number>;
 
-    // Latest irreverisble blockmesh layer info
+    // Returns the latest irreverisble mesh layer number
     GetLatestIrreversibleLayerNumber(): Promise<number>;
 
-    // Get an account balance
-    GetBalance(account: Account, layer: ILayer): Promise<number>;
+    // Gets an account balance
+    GetBalance(account: string, layer: ILayer): Promise<number>;
 
     // Get the transactions count for this account. e.g. account nonce
-    GetTransactionsCount(account: Account, layer: ILayer): Promise<number>;
-
-    // Fiat exchange rate oracle
-    GetSMCFiatValue(): Promise<number>;
+    GetTransactionsCount(account: string, layer: ILayer): Promise<number>;
 
     // send a signed transaction, returns tx hash
     SendSignedTransaction(tx:ITransaction): Promise<TransactionResult>
 
-    // Get a transaction from the blockmesh (Tx will include layer number and whether it is reversible or not)
+    // Gets a transaction from the blockmesh (Tx will include layer number and whether it is reversible or not)
     GetTransaction(txHash: string): Promise<TransactionResult>;
 
+    // Subscribe a callback to recieve tx status changes
     SubscribeToTxStatus(txHash: string, callback: (TransactionStatus) => void ) : Promise<boolean>;
-
     UnsubscribeFromTxStatus(txHash: string): Promise<boolean>;
+
+    // Subsribe to get called back when a transaction for the user is on the mesh
+    SubscribeToIncomingTxs(account: string, callback: (TransactionStatus) => void ) : Promise<boolean>;
+
+    // Unbsuscribe from incoming transactions for the user
+    UnsubscribeFromIncomingTxs(account: string): Promise<boolean>;
+
+    // Fiat exchange rate oracle
+    GetSMCFiatValue(): Promise<number>;
 
 }
